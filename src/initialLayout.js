a28e2564a21068b4e641c41708c1cf4602af2a11
Jesse <jessepullen@icloud.com>
2024-05-20 21:30:02 +1000

    Create and style home page

---
+++ src/initialLayout.js
@@ -0,0 +1,20 @@
+import './style.css';
+
+function initialLayout() {
+    const content = document.querySelector('#content');
+
+    const banner = document.createElement('div');
+    const title = document.createElement('h1');
+    const headline = document.createElement('h3');
+
+    banner.classList.add('banner');
+
+    title.textContent = 'Sizzle & Surf';
+    headline.textContent = 'Steak & Seafood';
+
+    content.appendChild(banner);
+    banner.appendChild(title);
+    banner.appendChild(headline);
+}
+
+export { initialLayout };
